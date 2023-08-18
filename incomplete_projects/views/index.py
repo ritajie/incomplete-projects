21ad218580a5476b99d73b9876600941a1873e70
路小鹿 <1551755561@qq.com>
2023-08-18 17:31:30 +0800

    Add backup-db.sql

--- incomplete_projects/views/index.py
+++ incomplete_projects/views/index.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, "fuck.html")
-    # return render(request, "index.html")
+    # return render(request, "fuck.html")
+    return render(request, "index.html")
